320749df38f2a7fa6ff4e287de2beb6aee7e987a
Anton <komogorcevanton10@gmail.com>
2026-05-09 21:12:20 +0800

    feat: added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+
 import "./global.css";
 import "./reset.css";
 
+import { Home } from "./pages/Home";
+
 export const App = () => {
-  return <></>;
+  return (
+    <section className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </section>
+  );
 };
