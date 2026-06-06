2bd7deb81bd34b672a15def5c5c033bb2db61607
Ефим <@DESKTOP-9RFE0S1>
2026-06-06 16:00:52 +0300

    added marquee

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -2,6 +2,7 @@ import { AboutUs } from "./sections/AboutUs";
 import { Features } from "./sections/Features";
 import { Hero } from "./sections/Hero";
 import { Information } from "./sections/Information";
+import { Marquee } from "./sections/Marquee";
 import { OurProducts } from "./sections/OurProducts";
 
 import "./styles/home-page.css";
@@ -12,8 +13,9 @@ export const HomePage = () => {
       <Hero />
       <AboutUs />
       <Features />
-      <Information />
       <OurProducts />
+      <Marquee />
+      <Information />
     </main>
   );
 };
