4e9bd0185f978d39ac96c1ab8b9bc527e60b39d9
Anton <komogorcevanton10@gmail.com>
2026-07-18 15:15:59 +0800

    feat: added Our History section

--- src/pages/about-us/AboutUsPage.tsx
+++ src/pages/about-us/AboutUsPage.tsx
@@ -1,5 +1,6 @@
 import { AboutUs } from "../../components/AboutUs";
 import { Founders } from "./sections/Founders";
+import { OurHistory } from "./sections/OurHistory";
 
 export const AboutUsPage = () => {
   return (
@@ -7,6 +8,7 @@ export const AboutUsPage = () => {
       <section className="app-pages">
         <AboutUs />
         <Founders />
+        <OurHistory />
       </section>
     </>
   );
---
+++ src/pages/about-us/sections/OurHistory.tsx
@@ -0,0 +1,48 @@
+import dateLine from "/about-us/our-history/date-line.svg";
+
+import "./styles/our-history.css";
+
+interface ActionByDate {
+  date: number;
+  action: string;
+}
+
+const ACTIONS: ActionByDate[] = [
+  {
+    date: 2018,
+    action:
+      "Ut non dolor anim cupidatat sunt pariatur labore ex excepteur labore laborum sit Lorem. Dolor non proident amet in exercitation aute anim reprehende",
+  },
+  {
+    date: 2020,
+    action:
+      "Ut non dolor anim cupidatat sunt pariatur labore ex excepteur labore laborum sit Lorem. Dolor non proident amet in exercitation aute anim reprehende",
+  },
+  {
+    date: 2022,
+    action:
+      "Ut non dolor anim cupidatat sunt pariatur labore ex excepteur labore laborum sit Lorem. Dolor non proident amet in exercitation aute anim reprehende",
+  },
+];
+
+export const OurHistory = () => {
+  return (
+    <section className="our-history">
+      <section className="our-history-head">
+        <p className="description">Dolore dolore voluptate aliqua ut mi</p>
+        <h2>Our History</h2>
+      </section>
+      <section className="our-history-body">
+        <img src={dateLine} alt="" />
+        <section>
+          {ACTIONS.map((action) => (
+            <div className="date-action">
+              <h3>{action.date}</h3>
+              <p>{action.action}</p>
+            </div>
+          ))}
+        </section>
+      </section>
+    </section>
+  );
+};
