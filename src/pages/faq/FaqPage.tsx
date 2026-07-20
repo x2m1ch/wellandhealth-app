6af07ec581846e3e8437704a13f0d6fec45e1e1c
Anton <komogorcevanton10@gmail.com>
2026-07-20 09:58:59 +0800

    feat: added the Get section

--- src/pages/faq/FaqPage.tsx
+++ src/pages/faq/FaqPage.tsx
@@ -1,10 +1,14 @@
 import { Footer } from "../../components/Footer";
+import { Get } from "../../components/Get";
 import { GetInTouch } from "../../components/GetInTouch";
 import { Ask } from "./sections/Ask";
 
 export const FaqPage = () => {
   return <>
     <Ask />
+    <section className="app-pages">
+      <Get heading="Still have question?"/>
+    </section>
     <GetInTouch />
     <Footer />
   </>;
