db2c2aeea71f573c6747677e211d8bf9fdb20037
Anton <komogorcevanton10@gmail.com>
2026-07-21 08:53:45 +0800

    feat: added the Asks section

--- src/pages/faq/FaqPage.tsx
+++ src/pages/faq/FaqPage.tsx
@@ -1,12 +1,14 @@
 import { Footer } from "../../components/Footer";
 import { Get } from "../../components/Get";
 import { GetInTouch } from "../../components/GetInTouch";
-import { Ask } from "./sections/Ask";
+import { Ask } from "./sections/AskHero";
+import { Asks } from "./sections/Asks";
 
 export const FaqPage = () => {
   return <>
     <Ask />
     <section className="app-pages">
+      <Asks />
       <Get heading="Still have question?"/>
     </section>
     <GetInTouch />
--- src/pages/faq/sections/AskHero.tsx
+++ src/pages/faq/sections/AskHero.tsx
@@ -1,13 +1,13 @@
-import search from "/faq/ask/search.svg";
-import background from "/faq/ask/background.png";
+import search from "/faq/ask-hero/search.svg";
+import background from "/faq/ask-hero/background.png";
 
-import "./styles/ask.css";
+import "./styles/ask-hero.css";
 
 export const Ask = () => {
   return (
-    <section className="ask">
+    <section className="ask-hero">
       <img src={background} alt="" />
-      <section className="ask-body">
+      <section className="ask-hero-body">
         <h2>Frequently asked questions</h2>
         <div>
           <img src={search} alt="" />
