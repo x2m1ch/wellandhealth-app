e8e90da1951fc49fded168cd45eae4288ab360ff
Anton <komogorcevanton10@gmail.com>
2026-07-22 12:17:51 +0800

    feat: added info section

--- src/pages/help-center/HelpCenterPage.tsx
+++ src/pages/help-center/HelpCenterPage.tsx
@@ -1,7 +1,11 @@
 import { Hero } from "./sections/Hero";
+import { Info } from "./sections/Info";
 
 export const HelpCenterPage = () => {
   return <>
     <Hero />
+    <section className="app-pages">
+        <Info />
+    </section>
   </>;
 };
